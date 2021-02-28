e373acad6b78a52c112cea07ceb7bc6310ef33a6
kmcgee92 <kaseycmcgee@gmail.com>
2021-02-28 15:26:22 -0700

    test footer

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,14 @@ const App = () => {
       <LandingPage />
       <h1>Kenny's basic ass website</h1>
       <img src="https://entrepreneurhandbook.co.uk/wp-content/uploads/2014/03/3d-printer.jpg"></img>
+      <div className="footer">
+        <div>Support me! Footer</div>
+        <div>email maybe</div>
+        <div>phone maybe</div>
+      </div>
     </>
-  );;;
-};
+  );
+};;
 
 export default App;
 
