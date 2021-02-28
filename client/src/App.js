77add1ecf65543a395038a390baca3a70d86fb72
kmcgee92 <kaseycmcgee@gmail.com>
2021-02-28 15:19:26 -0700

    test image

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,13 @@
 import LandingPage from "./Components/LandingPage"
 const App = () => {
-
   return (
     <>
-      <LandingPage/>
-      <h1>
-        Kenny's basic ass website
-      </h1>
+      <LandingPage />
+      <h1>Kenny's basic ass website</h1>
+      <img src="https://entrepreneurhandbook.co.uk/wp-content/uploads/2014/03/3d-printer.jpg"></img>
     </>
-  )}
+  );;;
+};
 
 export default App;
 
