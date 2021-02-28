c61bbd91fb8160fd5a2d3359b6dc545e116f034d
kmcgee92 <kaseycmcgee@gmail.com>
2021-02-28 14:58:46 -0700

    baseline website template

--- client/src/App.js
+++ client/src/App.js
@@ -1,83 +1,84 @@
 
-import React, { useEffect } from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Cookies from "js-cookie";
-// redux
-import { useDispatch, useSelector } from "react-redux";
-import { refreshSession } from "./redux/actions/authActions";
-import { refreshList } from "./redux/actions/userTodoActions";
-// apollo
-import { useLazyQuery } from "@apollo/react-hooks";
-import { GET_ACTIVE_USER } from "./graphql/queries/getActiveUser";
-// core components
-import TodoInterface from "./Components/TodoInterface/TodoInterface"
-//mui components
-import { makeStyles } from "@material-ui/core/styles";
-// styles
-import { TodoStyles } from "./MUIStyles";
+// import React, { useEffect } from "react";
+// import { BrowserRouter, Switch, Route } from "react-router-dom";
+// import Cookies from "js-cookie";
+// // redux
+// import { useDispatch, useSelector } from "react-redux";
+// import { refreshSession } from "./redux/actions/authActions";
+// import { refreshList } from "./redux/actions/userTodoActions";
+// // apollo
+// import { useLazyQuery } from "@apollo/react-hooks";
+// import { GET_ACTIVE_USER } from "./graphql/queries/getActiveUser";
+// // core components
+// import TodoInterface from "./Components/TodoInterface/TodoInterface"
+// //mui components
+// import { makeStyles } from "@material-ui/core/styles";
+// // styles
+// import { TodoStyles } from "./MUIStyles";
 
 const App = () => {
 
-    const interactiveDrawer = "80px";
-    const useStyles = makeStyles((styles) =>
-      TodoStyles(styles, "180px", interactiveDrawer)
-    );
-    const classes = useStyles();
+  //   const interactiveDrawer = "80px";
+  //   const useStyles = makeStyles((styles) =>
+  //     TodoStyles(styles, "180px", interactiveDrawer)
+  //   );
+  //   const classes = useStyles();
 
 
     
-  const dispatch = useDispatch();
-  const auth = useSelector((state) => state.auth);
+  // const dispatch = useDispatch();
+  // const auth = useSelector((state) => state.auth);
 
-  const [getActiveUser, { data, loading, error }] = useLazyQuery(
-    GET_ACTIVE_USER
-  );
-  // OnMount will check to see if session needs to be refreshed
-  useEffect(() => {
-    const activeToken = Cookies.get("token");
-    if (activeToken) {
-      if (!Object.keys(auth.activeUser).length) {
-        getActiveUser({
-          variables: {
-            token: activeToken,
-          },
-          fetchPolicy: "network-only",
-        });
-      }
-    }
-  }, [getActiveUser, auth.activeUser]);
+  // const [getActiveUser, { data, loading, error }] = useLazyQuery(
+  //   GET_ACTIVE_USER
+  // );
+  // // OnMount will check to see if session needs to be refreshed
+  // useEffect(() => {
+  //   const activeToken = Cookies.get("token");
+  //   if (activeToken) {
+  //     if (!Object.keys(auth.activeUser).length) {
+  //       getActiveUser({
+  //         variables: {
+  //           token: activeToken,
+  //         },
+  //         fetchPolicy: "network-only",
+  //       });
+  //     }
+  //   }
+  // }, [getActiveUser, auth.activeUser]);
 
-  useEffect(() => {
-    if (data) {
-      if (!data.error) {
-        dispatch(refreshSession(data));
-      }
-    }
-  }, [dispatch, data, loading, error]);
+  // useEffect(() => {
+  //   if (data) {
+  //     if (!data.error) {
+  //       dispatch(refreshSession(data));
+  //     }
+  //   }
+  // }, [dispatch, data, loading, error]);
 
-  useEffect(() => {
-    if (Object.keys(auth.activeUser).length) {
-      dispatch(refreshList(auth.activeUser.items));
-    }
-  }, [dispatch, auth, data]);
+  // useEffect(() => {
+  //   if (Object.keys(auth.activeUser).length) {
+  //     dispatch(refreshList(auth.activeUser.items));
+  //   }
+  // }, [dispatch, auth, data]);
 
-  if (error) {
-    return <h1>Database isn't connected properly or there is an error.</h1>;
-  }
-  if (loading) {
-    return <h1>Loading...</h1>;
-  }
+  // if (error) {
+  //   return <h1>Database isn't connected properly or there is an error.</h1>;
+  // }
+  // if (loading) {
+  //   return <h1>Loading...</h1>;
+  // }
 
   return (
-    <div>
-        <BrowserRouter>
+    <h1>
+      Kenny's basic ass website
+        {/* <BrowserRouter>
           <Switch>
             <Route exact path="/">
               <TodoInterface classes={classes} />
             </Route>
           </Switch>
-        </BrowserRouter>
-    </div>
+        </BrowserRouter> */}
+    </h1>
   );
 };;
 
