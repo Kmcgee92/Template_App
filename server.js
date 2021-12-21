49e600109974e28b5a8c0c7176dbf241b0b8748d
kmcgee92 <kaseycmcgee@gmail.com>
2021-12-20 19:29:20 -0700

    test server deploy

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
 
-const port = process.env.port || 5000 ;
+const port = process.env.PORT || 5000 ;
 app.listen(port, () => {
   console.log(`serving react on http://localhost:${port}`);
 });
