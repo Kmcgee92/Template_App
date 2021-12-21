d77cd8f5df6649aa31f5822655d12cbc4f0e12e7
kmcgee92 <kaseycmcgee@gmail.com>
2021-12-20 19:33:37 -0700

    test server deploy

--- server.js
+++ server.js
@@ -4,15 +4,14 @@ const app = express();
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, "./client/build")));
+// All other GET requests not handled before will return our React app
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
-});
-
-// All other GET requests not handled before will return our React app
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
 
 const port = process.env.PORT || 5000 ;
